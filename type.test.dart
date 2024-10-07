a975b70ed9a7fdbc217c2a991d778fa28ae5d4d3
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:50:08 +0700

    Create type.test.dart

---
+++ type.test.dart
@@ -0,0 +1,18 @@
+//INI ADALAH OPERATOR TYPE TEST
+ void main(){
+   dynamic variable = 100;
+
+  // var variableString = variable as String;
+   var variableInt = variable as int;
+
+  print(variable);
+   print(variableInt);
+
+   print(variable is int);
+   print(variable is bool);
+   print(variable is String);
+
+   print(variable is! int);
+   print(variable is! bool);
+   print(variable is! String);
+ }
